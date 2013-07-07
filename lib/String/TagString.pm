4dcf3b46ee2fe026c4c302e6a4175181a3335c3a
Ricardo Signes <rjbs@cpan.org>
2013-07-07 07:41:16 -0400

    dzilify

--- lib/String/TagString.pm
+++ lib/String/TagString.pm
@@ -1,7 +1,7 @@
 use warnings;
 use strict;
 package String::TagString;
-# ABSTRACT - parse and emit tag strings (including tags with values)
+# ABSTRACT: parse and emit tag strings (including tags with values)
 our $VERSION = '0.003';
 
 =head1 SYNOPSIS
@@ -56,9 +56,7 @@ A value is similar, but cannot start with an at sign.
 Alternately, either a tag or a value can be almost anything if it enclosed in
 double quotes.  (Internal double quotes can be escaped with a backslash.)
 
-=head1 METHODS
-
-=head2 tags_from_string
+=method tags_from_string
 
   my $tag_hashref = String::TagString->tags_from_string($tag_string);
 
@@ -118,13 +116,13 @@ sub tags_from_string {
 
     $tag{ $tag } = $value;
   }
-  
+
   die "invalid tagstring" unless defined $pos and $pos == length $tagstring;
 
   return \%tag;
 }
 
-=head2 string_from_tags
+=method string_from_tags
 
   my $string = String::TagString->string_from_tags( $tag_set );
 
@@ -171,25 +169,5 @@ sub string_from_tags {
 
   return join q{ }, @tags;
 }
-
-
-=head1 AUTHOR
-
-Ricardo Signes, C<< <rjbs@cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
-notified of progress on your bug as I make changes.
-
-=head1 COPYRIGHT
-
-Copyright 2006 Ricardo Signes, All Rights Reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-=cut
 
 1;
